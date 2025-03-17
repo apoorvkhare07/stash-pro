f38b96a1335a9e8c33b72184bd63ba2448355a04
Apoorv <apoorvkhare007@gmail.com>
2025-03-17 15:29:06 +0530

    add daily sales api

--- sales/views.py
+++ sales/views.py
@@ -3,8 +3,9 @@ from rest_framework import viewsets
 from .models import Sale
 from .serializers import SaleSerializer
 from rest_framework.response import Response
-from django.db.models import Sum, F, ExpressionWrapper, DurationField
+from django.db.models import Sum, F, ExpressionWrapper, DurationField, Count, DateField
 from django.utils.timezone import now
+from datetime import datetime, timedelta
 
 
 class SaleViewSet(viewsets.ModelViewSet):
@@ -32,6 +33,53 @@ class SaleViewSet(viewsets.ModelViewSet):
             "total_unshipped_items": total_unshipped_items,
             "sales": serializer.data,
         }
+
+        return Response(response_data)
+
+    @action(detail=False, methods=["get"])
+    def daily_sales(self, request):
+        """
+        Returns daily sales data from the start of current month up to current date.
+        """
+        # Get the first day of current month and current date
+        today = now()
+        first_day = today.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+        current_date = today.date()
+
+        # Get all sales from start of month to current date
+        sales_data = Sale.objects.filter(
+            sale_date__gte=first_day,
+            sale_date__lte=today
+        ).annotate(
+            date=ExpressionWrapper(
+                F('sale_date__date'),
+                output_field=DateField()
+            )
+        ).values('date').annotate(
+            total_sales=Count('id'),
+            total_amount=Sum(F('quantity_sold') * F('sale_price'))
+        ).order_by('date')
+
+        # Create a list of dates from start of month to current date
+        all_dates = []
+        current = first_day.date()
+        while current <= current_date:
+            all_dates.append(current)
+            current += timedelta(days=1)
+
+        # Create response data with all dates, including those with no sales
+        response_data = []
+        for date in all_dates:
+            date_str = date.strftime("%Y-%m-%d")
+            sales_for_date = next(
+                (item for item in sales_data if item['date'] == date),
+                {'total_sales': 0, 'total_amount': 0}
+            )
+            response_data.append({
+                "date": date_str,
+                "total_sales": sales_for_date['total_sales'],
+                "total_amount": float(sales_for_date['total_amount'] or 0)
+            })
 
         return Response(response_data)
 
