6f2f078d9c4b70a157ec4aeaac47fc933cf6aab3
Apoorv <apoorvkhare007@gmail.com>
2025-07-06 16:41:10 +0530

    fix load dotenv

--- stash_pro/settings.py
+++ stash_pro/settings.py
@@ -13,14 +13,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from urllib.parse import urlparse
-import dj_database_url
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
-# Load environment variables from .env file
-from dotenv import load_dotenv
-load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
